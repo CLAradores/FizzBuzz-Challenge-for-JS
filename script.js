121215c0927b5e3e52d11007206a7b9ee69b5480
charles1stgithub <charleslester.official@gmail.com>
2023-09-29 09:53:27 +0800

    FizzBuzz Code Challenge Solution

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 let log = 1;
 
 while (log <= 100) {
-  if (log % 3 === 0) {
-    console.log(`${log} Fizz`);
-  } else if (log % 5 === 0) {
-    console.log(`${log} Buzz`);
-  }
   if (log % 3 === 0 && log % 5 === 0) {
     console.log(`${log} FizzBuzz`);
+  } else if (log % 5 === 0) {
+    console.log(`${log} Buzz`);
+  } else if (log % 3 === 0) {
+    console.log(`${log} Fizz`);
   } else {
     console.log(log);
   }
